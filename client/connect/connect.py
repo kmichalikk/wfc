6f791ddb0d6c9067a24c634c0c181d0e346fa99e
Konrad Michalik <kmichalikk19@gmail.com>
2024-03-22 16:05:32 +0100

    simplify connect

--- client/connect/connect.py
+++ client/connect/connect.py
@@ -2,12 +2,14 @@ import pickle
 import panda3d.core as p3d
 from direct.distributed.PyDatagram import PyDatagram
 from direct.distributed.PyDatagramIterator import PyDatagramIterator
+from direct.showbase.DirectObject import DirectObject
 from direct.task.TaskManagerGlobal import taskMgr
 from common.typings import Messages
 
 
-class Connection:
+class Connection(DirectObject):
     def __init__(self):
+        super().__init__()
         self.connection_manager = p3d.QueuedConnectionManager()
         self.connection_reader = p3d.QueuedConnectionReader(self.connection_manager, 0)
         self.connection_writer = p3d.ConnectionWriter(self.connection_manager, 0)
@@ -20,38 +22,31 @@ class Connection:
             self.connection_reader.add_connection(self.connection)
 
     def find_room(self, callback):
-        def confirm_accept_room(data):
-            if data.get_uint8() == Messages.ACCEPT_ROOM_OK:
-                print("room accepted")
-                self.tiles = pickle.loads(data.get_blob())
-                callback()
-                return True
+        def poll(task):
+            if not self.connection_reader.data_available():
+                return task.cont
 
-        def accept_room(data):
-            if data.get_uint8() == Messages.FIND_ROOM_OK:
-                print("accept room")
+            datagram = p3d.NetDatagram()
+            if not self.connection_reader.get_data(datagram):
+                return task.cont
+
+            data = PyDatagramIterator(datagram)
+            message = data.get_uint8()
+            if message == Messages.FIND_ROOM_OK:
                 accept_datagram = PyDatagram()
                 accept_datagram.add_uint8(Messages.ACCEPT_ROOM)
                 self.connection_writer.send(accept_datagram, self.connection)
-                self.poll_callback(confirm_accept_room)
-                return True
+            elif message == Messages.ACCEPT_ROOM_OK:
+                self.tiles = pickle.loads(data.get_blob())
+                callback()
+                return task.done
 
-        print("find room")
+            return task.cont
+
         join_datagram = PyDatagram()
         join_datagram.add_uint8(Messages.FIND_ROOM)
         self.connection_writer.send(join_datagram, self.connection)
-        self.poll_callback(accept_room)
-
-    def poll_callback(self, callback):
-        def poll(task):
-            if self.connection_reader.data_available():
-                datagram = p3d.NetDatagram()
-                if self.connection_reader.get_data(datagram):
-                    data = PyDatagramIterator(datagram)
-                    callback(data)
-                    return
-            return task.cont
-        taskMgr.add(poll, "poll until callback is true")
+        taskMgr.add(poll, "find room")
 
     def poll_reader(self, task):
         if self.connection_reader.data_available():
