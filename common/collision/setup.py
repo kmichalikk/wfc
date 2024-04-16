fa693303327389b3fa86fda4fc03c1396c380e81
Konrad Michalik <kmichalikk19@gmail.com>
2024-04-16 18:38:45 +0200

    fix things after cherry-picking

--- common/collision/setup.py
+++ common/collision/setup.py
@@ -13,6 +13,9 @@ def setup_collisions(game, tiles, map_size, bullet_factory):
     game.collision_event_generator = p3d.CollisionHandlerEvent()
     game.collision_event_generator.add_in_pattern('%fn-into-%in')
 
+    for bullet in bullet_factory.bullets:
+        game.cTrav.add_collider(bullet.colliders[0], game.collision_event_generator)
+
     game.border = Border(game.render, map_size)
     game.tile_colliders = []
 
@@ -21,7 +24,12 @@ def setup_collisions(game, tiles, map_size, bullet_factory):
     for tile_data in tiles:
         tile = create_new_tile(game.loader, tile_data["node_path"], tile_data["pos"], tile_data["heading"])
         tile.reparent_to(tiles_parent)
-        game.tile_colliders.append(CollisionObject(tile, tile_data["node_path"],
-                                                   collision_shapes[tile_data["node_path"]]))
+        game.tile_colliders.append(
+            CollisionObject(
+                tile,
+                "wall" if "wall" in tile_data["node_path"] else tile_data["node_path"],
+                collision_shapes[tile_data["node_path"]]
+            )
+        )
     tiles_parent.flatten_strong()
     tiles_parent.reparent_to(game.render)
