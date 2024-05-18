06f4c403d092e5fa04bfdb7b0fd174d5a7db4a9b
Gabriela-Dumanska <dumanska@student.agh.edu.pl>
2024-05-19 00:02:57 +0200

    Privacy changes

--- client/main.py
+++ client/main.py
@@ -44,15 +44,15 @@ class Game(ShowBase):
         # initialize GameManager, don't start the game until self.ready
         self.game_manager = GameManager(self, TileNodePathFactory(self.loader))
 
-        self._bolts_set_up = False
-        self._bolt_factory = BoltFactory(self.loader, self.render)
+        self.__bolts_set_up = False
+        self.__bolt_factory = BoltFactory(self.loader, self.render)
         self.connection_manager.subscribe_for_game_end(self.game_manager.game_end_handler)
 
     def start(self, username):
-        self.connection_manager.wait_for_connection(self._ready_handler, username)
-        self.connection_manager.subscribe_for_new_player(self._new_player_handler)
+        self.connection_manager.wait_for_connection(self.__ready_handler, username)
+        self.connection_manager.subscribe_for_new_player(self.__new_player_handler)
 
-    def _ready_handler(self, game_config: GameConfig):
+    def __ready_handler(self, game_config: GameConfig):
         self.expected_players = game_config.expected_players
         self.game_manager.setup_map(self, game_config.tiles, game_config.size, game_config.season)
         for state in game_config.player_states:
@@ -61,29 +61,29 @@ class Game(ShowBase):
                 self.game_manager.set_main_player(player)
 
         # now the game is ready to start, so we start listening for server changes
-        self.connection_manager.subscribe_for_new_player(self._new_player_handler)
-        self.connection_manager.subscribe_for_game_state_change(self._game_state_change)
+        self.connection_manager.subscribe_for_new_player(self.__new_player_handler)
+        self.connection_manager.subscribe_for_game_state_change(self.__game_state_change)
 
-    def _game_state_change(self, game_state_transfer: NetworkTransfer):
+    def __game_state_change(self, game_state_transfer: NetworkTransfer):
         self.game_manager.queue_server_game_state(game_state_transfer)
 
-    def _new_player_handler(self, player_state: PlayerStateDiff):
+    def __new_player_handler(self, player_state: PlayerStateDiff):
         print("[INFO] New player {}".format(player_state.username))
         self.game_manager.setup_player(player_state)
 
     def attach_input(self):
-        self.accept("w", lambda: self._handle_input("+forward"))
-        self.accept("w-up", lambda: self._handle_input("-forward"))
-        self.accept("s", lambda: self._handle_input("+backward"))
-        self.accept("s-up", lambda: self._handle_input("-backward"))
-        self.accept("d", lambda: self._handle_input("+right"))
-        self.accept("d-up", lambda: self._handle_input("-right"))
-        self.accept("a", lambda: self._handle_input("+left"))
-        self.accept("a-up", lambda: self._handle_input("-left"))
-        self.accept("space-up", lambda: self._handle_bullet())
-        self.accept("q", lambda: self._handle_flag_drop())
+        self.accept("w", lambda: self.__handle_input("+forward"))
+        self.accept("w-up", lambda: self.__handle_input("-forward"))
+        self.accept("s", lambda: self.__handle_input("+backward"))
+        self.accept("s-up", lambda: self.__handle_input("-backward"))
+        self.accept("d", lambda: self.__handle_input("+right"))
+        self.accept("d-up", lambda: self.__handle_input("-right"))
+        self.accept("a", lambda: self.__handle_input("+left"))
+        self.accept("a-up", lambda: self.__handle_input("-left"))
+        self.accept("space-up", lambda: self.__handle_bullet())
+        self.accept("q", lambda: self.__handle_flag_drop())
 
-    def _handle_bullet(self):
+    def __handle_bullet(self):
         direction = self.game_manager.shoot_bullet()
         if direction is None:
             return
@@ -95,7 +95,7 @@ class Game(ShowBase):
         self.taskMgr.do_method_later(0, lambda _: self.connection_manager.send_flag_trigger(player.get_id()),
                                      "send input on next frame")
 
-    def _handle_flag_drop(self):
+    def __handle_flag_drop(self):
         player = self.game_manager.main_player
         self.taskMgr.do_method_later(0, lambda _: self.connection_manager.send_flag_drop_trigger(player.get_id()),
                                      "send input on next frame")
@@ -109,14 +109,14 @@ class Game(ShowBase):
         self.flag.get_dropped(player)
 
     def setup_bolts(self, current_bolts):
-        if self._bolts_set_up:
+        if self.__bolts_set_up:
             return
-        self._bolts_set_up = True
-        self._bolt_factory.undump_bolts(current_bolts)
+        self.__bolts_set_up = True
+        self.__bolt_factory.undump_bolts(current_bolts)
 
     def update_bolts(self, old_bolt_id, new_bolt):
-        self._bolt_factory.remove_bolt(old_bolt_id)
-        self._bolt_factory.copy_bolts([new_bolt])
+        self.__bolt_factory.remove_bolt(old_bolt_id)
+        self.__bolt_factory.copy_bolts([new_bolt])
 
     def pick_bolt(self, entry):
         bolt_id = entry.getIntoNodePath().node().getName()[-1]
@@ -126,7 +126,7 @@ class Game(ShowBase):
         self.taskMgr.do_method_later(0, lambda _: self.connection_manager.send_bolt_pickup_trigger(bolt_id),
                                      "send input on next frame")
 
-    def _handle_input(self, input: Input):
+    def __handle_input(self, input: Input):
         self.game_manager.main_player.update_input(input)
         # send input to server on next frame in order to give ourselves a change
         # to respond to input before server sees it
